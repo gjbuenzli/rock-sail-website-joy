f654b4e519643bab2a876033233bfe8897b9aae0
gjbuenzli <gbuenzli@gmail.com>
2025-07-11 23:43:27 +0000

    base again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '',
+  base: '/rock-sail-website-joy',
   server: {
     host: "::",
     port: 8080,
