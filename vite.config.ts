78b513d69cb0fa2e4e2c6ecb3b83cbe754e53a80
gjbuenzli <gbuenzli@gmail.com>
2025-07-11 22:54:28 +0000

    new base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/',
+  base: '/rock-sail-website-joy',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
