588399b37d63ae0834d82fac164c9053f4e34d60
gjbuenzli <gbuenzli@gmail.com>
2025-07-12 00:13:47 +0000

    cross fingers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,29 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import  Index  from "./pages/Index.tsx"
 
 createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <Index />,
+      },
+      {
+        path: "/vite-react-router/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
